d9d9654658cf5f2c53ba9403e3fff35161d4a855
Corey Barr <coreybarr01@gmail.com>
2026-04-28 16:54:23 +1000

    feat: add neovim keymaps

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,19 +1,41 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- ═══════════════════════════════════════════════════════════
+-- SEARCH & NAVIGATION
+-- ═══════════════════════════════════════════════════════════
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+-- Select all content
+vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true, desc = 'Select all' })
+
+-- Copy whole file to clipboard
+vim.keymap.set('n', '<C-c>', ':%y+<CR>', { noremap = true, silent = true })
+
+-- ═══════════════════════════════════════════════════════════
+-- TEXT EDITING
+-- ═══════════════════════════════════════════════════════════
 
 -- Move lines up/down
 vim.keymap.set('n', '<A-j>', "<cmd>execute 'move .+' . v:count1<cr>==", { desc = 'Move Down' })
 vim.keymap.set('n', '<A-k>', "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = 'Move Up' })
+
+-- Commenting (add comment above/below current line)
+vim.keymap.set('n', '<leader>cb', 'o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add [C]omment [B]elow' })
+vim.keymap.set('n', '<leader>ca', 'O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>', { desc = 'Add [C]omment [A]bove' })
+
+-- ═══════════════════════════════════════════════════════════
+-- FILE OPERATIONS
+-- ═══════════════════════════════════════════════════════════
+
+-- Save file (works in all modes)
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<cr><esc>', { desc = 'Save File' })
+
+-- ═══════════════════════════════════════════════════════════
+-- WINDOW MANAGEMENT (splitting and navigation)
+-- ═══════════════════════════════════════════════════════════
 
 -- Keybinds to make split navigation easier
 --  Use CTRL+<hjkl> to switch between windows
@@ -23,7 +45,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
--- [[ Basic Autocommands ]]
+-- ═══════════════════════════════════════════════════════════
+-- AUTO COMMANDS
+-- ═══════════════════════════════════════════════════════════
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
@@ -33,3 +57,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- ═══════════════════════════════════════════════════════════
+-- UTILITY
+-- ═══════════════════════════════════════════════════════════
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>w', '<cmd>set wrap!<CR>', { desc = 'Toggle [W]rap', silent = true })
