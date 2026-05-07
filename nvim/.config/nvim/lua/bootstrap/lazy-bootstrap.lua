8e2321b320de47fc926a0b449163193cec3856dc
Corey Barr <coreybarr01@gmail.com>
2026-05-07 11:41:18 +1000

    feat/nvim: update lazy-bootstrap.lua

--- nvim/.config/nvim/lua/bootstrap/lazy-bootstrap.lua
+++ nvim/.config/nvim/lua/bootstrap/lazy-bootstrap.lua
@@ -1,13 +1,17 @@
 -- [[ Install `lazy.nvim` plugin manager if not already existing ]]
 -- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
+-- Define where lazy should be
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 
+-- Install lazy if it does not exist
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
   if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
 end
 
+-- Add lazy to Neovim's runtimepath
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
