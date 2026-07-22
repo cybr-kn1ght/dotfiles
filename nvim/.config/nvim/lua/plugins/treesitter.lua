127a0552a3e8e7accb3f69311d2c2b1ccdf57869
Corey Barr <coreybarr01@gmail.com>
2026-07-22 11:34:25 +1000

    feat(nvim): update treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,13 @@
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
     build = ':TSUpdate',
     config = function()
-      local filetypes = {
+      local parsers = {
         'bash',
         'c',
+        'c_sharp',
         'diff',
         'html',
         'lua',
@@ -19,12 +20,30 @@ return {
         'javascript',
         'typescript',
         'tsx',
-        'c_sharp',
       }
-      require('nvim-treesitter').install(filetypes)
+
+      require('nvim-treesitter').install(parsers)
+
+      local filetypes = {
+        'bash',
+        'c',
+        'cs',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'vim',
+        'vimdoc',
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+      }
+
       vim.api.nvim_create_autocmd('FileType', {
         pattern = filetypes,
-        callback = function() vim.treesitter.start() end,
+        callback = function(args) pcall(vim.treesitter.start, args.buf) end,
       })
     end,
   },
